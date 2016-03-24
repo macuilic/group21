3387c6e2e5c20ff65e4160fd8ca4844d280581fc
Barry O'Connell <oconneba@tcd.ie>
2016-03-24 12:05:23 +0000

    Expandable list version 1

--- MyApplication/app/src/main/java/com/example/johnmilsom/ggleaguesv1local/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/johnmilsom/ggleaguesv1local/MainActivity.java
@@ -1,24 +1,31 @@
 package com.example.johnmilsom.ggleaguesv1local;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ExpandableListView;
+import android.widget.Toast;
 
-import layout.Profile;
+import java.util.ArrayList;
+
 import layout.Tournaments;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements /*NavigationView.OnNavigationItemSelectedListener,*/ExpandableListView.OnChildClickListener {
+
     NavigationView navigationView;
     Toolbar toolbar;
+    DrawerLayout drawer;
+    ExpandableListView drawerList;
+    ActionBarDrawerToggle toggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,17 +55,20 @@ public class MainActivity extends AppCompatActivity
             }
         });
         */
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
         getSupportActionBar().setTitle("Tournaments");
         //getSupportActionBar().setDisplayShowTitleEnabled(false);
         //getSupportActionBar().setLogo(R.drawable.ggleagues_white);
-        navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
 
+        //navigationView = (NavigationView) findViewById(R.id.nav_view);
+        //navigationView.setNavigationItemSelectedListener(this);
+
+        setGroupData();
+        setChildGroupData();
+        initDrawer();
 
     }
 
@@ -93,7 +103,7 @@ public class MainActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
+   /* @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
@@ -120,6 +130,91 @@ public class MainActivity extends AppCompatActivity
         }
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }*/
+
+    private void initDrawer() {
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        drawerList = (ExpandableListView) findViewById(R.id.left_drawer);
+
+        drawerList.setAdapter(new NewAdapter(this, groupItem, childItem));
+
+        drawerList.setOnChildClickListener(this);
+
+        // actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawer,
+        // R.drawable.ic_drawer, R.string.open_drawer,
+        // R.string.close_drawer) {
+        // public void onDrawerClosed(View view) {
+        // getActionBar().setSubtitle("open");
+        // }
+        //
+        // /** Called when a drawer has settled in a completely open state. */
+        // public void onDrawerOpened(View drawerView) {
+        // getActionBar().setSubtitle("close");
+        // }
+        //
+        // };
+        //
+        // drawer.setDrawerListener(actionBarDrawerToggle);
+
+    }
+
+    ArrayList<String> groupItem = new ArrayList<String>();
+    ArrayList<Object> childItem = new ArrayList<Object>();
+
+    public void setGroupData() {
+        groupItem.add("Tournaments");
+        groupItem.add("My Profile");
+        groupItem.add("Chat");
+        groupItem.add("Media Center");
+    }
+
+    public void setChildGroupData() {
+        /**
+         * Add Data For TecthNology
+         */
+        ArrayList<String> child = new ArrayList<String>();
+        child.add("Tournaments");
+        child.add("My Profile");
+        child.add("Chat");
+        child.add("Media Center");
+        childItem.add(child);
+
+        /**
+         * Add Data For Mobile
+         */
+        child = new ArrayList<String>();
+        child.add("Android");
+        child.add("Window Mobile");
+        child.add("iPHone");
+        child.add("Blackberry");
+        childItem.add(child);
+        /**
+         * Add Data For Manufacture
+         */
+        child = new ArrayList<String>();
+        child.add("HTC");
+        child.add("Apple");
+        child.add("Samsung");
+        child.add("Nokia");
+        childItem.add(child);
+        /**
+         * Add Data For Extras
+         */
+        child = new ArrayList<String>();
+        child.add("Contact Us");
+        child.add("About Us");
+        child.add("Location");
+        child.add("Root Cause");
+        childItem.add(child);
+    }
+
+    @Override
+    public boolean onChildClick(ExpandableListView parent, View v,
+                                int groupPosition, int childPosition, long id) {
+        Toast.makeText(this, "Clicked On Child" + v.getTag(),
+                Toast.LENGTH_SHORT).show();
         return true;
     }
 
