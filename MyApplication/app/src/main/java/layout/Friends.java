7edb7f60481f04e307b348a953826b7ac5b3cfd5
lgill <lgill@lgill-pc.local>
2016-03-31 00:56:44 +0100

    add swipe feature to profile round 2

---
+++ MyApplication/app/src/main/java/layout/Friends.java
@@ -0,0 +1,25 @@
+package layout;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.johnmilsom.ggleaguesv1local.R;
+
+
+public class Friends extends Fragment {
+    public Friends() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_friend_friends, container, false);
+    }
+}
