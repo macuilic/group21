f46bc9e3e74d11b964d45c8d28dc84d99d9dfb0f
John Milsom <John Milsom>
2016-04-01 12:28:50 +0100

    adding tournament list & viewer, fixed checkable behaviour for navigation drawer

--- MyApplication/app/src/main/java/layout/Tournaments.java
+++ MyApplication/app/src/main/java/layout/Tournaments.java
@@ -1,25 +1,34 @@
 package layout;
 
 
+import android.app.ActionBar;
 import android.content.ClipData;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.PagerTabStrip;
 import android.support.v4.view.PagerTitleStrip;
 import android.text.Html;
 import android.text.Layout;
+import android.util.Log;
 import android.view.Display;
+import android.view.Gravity;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TabHost;
@@ -43,7 +52,6 @@ public class Tournaments extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
-
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_tournament, container, false);
     }
@@ -51,20 +59,50 @@ public class Tournaments extends Fragment {
     @Override
     public void onActivityCreated(Bundle savedInstanceState){
         super.onActivityCreated(savedInstanceState);
-        GridLayout grid = (GridLayout) getView().findViewById(R.id.tourGrid);
+        LinearLayout tourList = (LinearLayout) getView().findViewById(R.id.tourList);
         final LinearLayout filterLayout = (LinearLayout)getView().findViewById(R.id.filters);
         filterLayout.setVisibility(View.GONE);
         setupFilters();
 
-        WindowManager wm = (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
-        Display display = wm.getDefaultDisplay();
-        Button[] myButtons = new Button[20];
-        for (Button n : myButtons){
-            n = new Button(getContext());
-            n.setText(Html.fromHtml("String I am testing<br/><small>(with a sub comment)</small>"));
-            n.setWidth(wm.getDefaultDisplay().getWidth() / 2);
-            n.setHeight(wm.getDefaultDisplay().getHeight() / 6);
-            grid.addView(n);
+
+        LinearLayout[] myButtons = new LinearLayout[20];
+        for (int i = 0; i < myButtons.length;i++){
+            final LinearLayout n = tourListItem("Name", "01/12/2016", "32/40", "EU");
+            tourList.addView(n);
+            n.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    boolean down = false;
+                    boolean up = false;
+                    boolean drag = false;
+                    switch (event.getAction()) {
+                        case MotionEvent.ACTION_DOWN:
+                            down = true;
+                            break;
+                        case MotionEvent.ACTION_UP:
+                            up = true;
+                            break;
+                        case MotionEvent.ACTION_MOVE:
+                            drag = true;
+                    }
+                    if (down && !drag) {
+                        n.setBackgroundColor(Color.LTGRAY);
+                    } else {
+                        n.setBackgroundColor(Color.TRANSPARENT);
+                    }
+
+                    if (up) {
+                        //open tournament fragment
+                        TournamentView tourFrag= new TournamentView();
+                        android.support.v4.app.FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.fragment_container, tourFrag);
+                        fragmentTransaction.addToBackStack("myscreen");         //adding to "backstack" for when back button is pressed
+                        fragmentTransaction.commit();
+                    }
+                    return false;
+                }
+            });
+            myButtons[i] = n;
         }
 
         final Button filterButton = (Button)getView().findViewById(R.id.filterButton);
@@ -106,8 +144,6 @@ public class Tournaments extends Fragment {
             newFilter.addView(newText);
             newFilter.addView(newSpinner);
         }
-
-
     }
 
     public class Filter{
@@ -124,5 +160,64 @@ public class Tournaments extends Fragment {
             return options;
         }
     }
+    public LinearLayout tourListItem(String name, String startDate, String players, String region){
+        WindowManager wm = (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
 
+
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        //overall clickable layout
+        LinearLayout item = new LinearLayout(getContext());
+        item.setLayoutParams(lp);
+        item.setOrientation(LinearLayout.VERTICAL);
+        item.setClickable(true);
+        LinearLayout info = new LinearLayout(getContext());
+        info.setLayoutParams(lp);
+        info.setOrientation(LinearLayout.HORIZONTAL);
+
+        //info
+        TextView nameView = new TextView(getContext());
+        nameView.setText(name);
+        nameView.setTextColor(Color.DKGRAY);
+        TextView dateView = new TextView(getContext());
+        dateView.setText(startDate);
+        TextView regionView = new TextView(getContext());
+        regionView.setText(region);
+        regionView.setTextColor(Color.DKGRAY);
+        regionView.setGravity(Gravity.RIGHT);
+        TextView playersView = new TextView(getContext());
+        playersView.setText(players);
+        playersView.setGravity(Gravity.RIGHT);
+
+        //individual layouts
+        LinearLayout.LayoutParams lphalf = new LinearLayout.LayoutParams(display.getWidth()/2-40,LinearLayout.LayoutParams.WRAP_CONTENT);
+        lphalf.setMargins(20,0,20,0);
+        LinearLayout nameDate = new LinearLayout(getContext());
+        nameDate.setOrientation(LinearLayout.VERTICAL);
+        nameDate.addView(nameView);
+        nameDate.addView(dateView);
+        nameDate.setLayoutParams(lphalf);
+
+        LinearLayout regionPlayers = new LinearLayout(getContext());
+        regionPlayers.setOrientation(LinearLayout.VERTICAL);
+        regionPlayers.addView(regionView);
+        regionPlayers.addView(playersView);
+        regionPlayers.setGravity(Gravity.RIGHT);
+        regionPlayers.setLayoutParams(lphalf);
+
+        //Divider
+        ImageView divider = new ImageView(getContext());
+        LinearLayout.LayoutParams lp2 = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 3);
+        lp2.setMargins(10, 10, 10, 10);
+        divider.setLayoutParams(lp2);
+        divider.setBackgroundColor(0xF0E0E0E0);
+
+        //adding all items
+        info.addView(nameDate);
+        info.addView(regionPlayers);
+        item.addView(info);
+        item.addView(divider);
+
+        return item;
+    }
 }
