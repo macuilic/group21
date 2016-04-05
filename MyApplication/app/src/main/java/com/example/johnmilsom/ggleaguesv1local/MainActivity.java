643a81c66e9254c934e6e6fe980ddbd00cfde5b9
John Milsom <John Milsom>
2016-04-05 11:52:33 +0100

    Finished front end for tournaments

--- MyApplication/app/src/main/java/com/example/johnmilsom/ggleaguesv1local/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/johnmilsom/ggleaguesv1local/MainActivity.java
@@ -10,6 +10,7 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -30,6 +31,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.v("TESTING", "----------------");
         setContentView(R.layout.activity_main);
 
         //Set the fragment initially
