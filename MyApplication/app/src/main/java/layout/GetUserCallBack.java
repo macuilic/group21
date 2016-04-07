24310e1009a0686b65fb67b76325a6d8865f05b4
Conor McQuillan <macuilic@tcd.ie>
2016-04-07 14:33:20 +0100

    Update GetUserCallBack.java

--- MyApplication/app/src/main/java/layout/GetUserCallBack.java
+++ MyApplication/app/src/main/java/layout/GetUserCallBack.java
@@ -2,7 +2,8 @@ package layout;
 
 /**
  * Created by Lotte on 29.03.2016.
- *
+ *  Acts as a listener to let the app now that a connection's been made
+ *  and that it's finished.
  */
 public interface GetUserCallBack {
     public  abstract void done(Contact returnedcontact);
